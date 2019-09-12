8dc45015930d45d6bc32b75da057c84a2b5d0afc
nkalra0123 <nkalra0123@gmail.com>
2019-09-12 11:21:26 +0530

    Adding h2 database, creating a classrepository Creating entity SchoolClass and adding class to db on post request on class

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,16 +1,39 @@
 package com.example.demo;
 
 
+import com.example.demo.model.SchoolClass;
+import com.example.demo.repository.ClassRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    ClassRepository classRepository;
+
     @GetMapping("/hello")
     public String hello()
     {
         return "<h1> Hello </h1>";
+    }
+
+    @PostMapping("/class")
+    public String addClass()
+    {
+        String name = "class1";
+        Integer id = 1;
+
+        SchoolClass schoolClass = new SchoolClass();
+        schoolClass.setId(id);
+        schoolClass.setClassName(name);
+
+        classRepository.save(schoolClass);
+
+        return "ok";
     }
 
 }
---
+++ src/main/java/com/example/demo/model/SchoolClass.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SchoolClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+
+    String className;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
